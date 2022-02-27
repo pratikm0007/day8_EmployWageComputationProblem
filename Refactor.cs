cc878aaa1e83626bac12be9beae1712d4d580496
pratikm0007 <pratikpmondal07@gmail.com>
2022-02-28 00:08:19 +0530

    push refactor code file

---
+++ Refactor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployWageComputationProblem
+{
+    public class Refactor
+    {
+        /// <summary>
+        /// Refactor code from UC1 to UC6
+        /// </summary>
+        public static void usingRefactor()
+        {
+            checkEmploy.EmployAbsentOrPresent();
+        }
+    }
+}
