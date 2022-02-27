5537d507519b6f8736b68f62f883a101dfec05ca
pratikm0007 <pratikpmondal07@gmail.com>
2022-02-27 23:56:50 +0530

    push Part time employ file

---
+++ PartTimeEmployWage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EmployWageComputationProblem
+{
+    public class PartTimeEmployWage
+    {
+        /// <summary>
+        /// Calculate Part Time Employ wage
+        /// </summary>
+        public int isPartTimeEmployWage;
+        public int isPartTimeHrs = 8;
+        public static void findPartTimeWage()
+        {
+            EmployWage obj2 = new EmployWage();
+            PartTimeEmployWage obj3 = new PartTimeEmployWage();
+
+            obj3.isPartTimeEmployWage = obj2.empWagePerHour * obj3.isPartTimeHrs;
+            Console.WriteLine("Part Time Employ Wage is : {0}", +obj3.isPartTimeEmployWage);
+            MonthlyWage.findMonthlyWage();
+        }
+    }
+}
