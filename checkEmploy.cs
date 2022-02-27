45871f21dc57de3ed3c047803d8b4482565ee721
pratikm0007 <pratikpmondal07@gmail.com>
2022-02-27 23:45:48 +0530

    puch check attendance file

---
+++ checkEmploy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace EmployWageComputationProblem
+{
+    public class checkEmploy
+    {
+        /// <summary>
+        /// Check employs are absent or present.
+        /// </summary>
+        public static void EmployAbsentOrPresent()
+        {
+            Random random = new Random();
+            int presenty = random.Next(2);
+
+            if (presenty == 1)
+            {
+                Console.WriteLine("Employ is Present");
+                EmployWage.CalculateEmpWage();
+            }
+            else if (presenty == 0)
+            {
+                Console.WriteLine("Employ is Absent");
+                Console.WriteLine("Employ per Day Wage is : 0");
+            }
+
+        }
+    }
+}
