fa7bf93f2b41e61813f6505eb429202f12e65d0c
pratikm0007 <pratikpmondal07@gmail.com>
2022-02-28 00:06:47 +0530

    push condition of wage file

---
+++ WagesCondition.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace EmployWageComputationProblem
+{
+    public class WagesCondition
+    {
+        /// <summary>
+        /// Wages condition
+        /// </summary>
+        public int hrs = 101;
+        public int dayHrs = 20 * 8;
+        public int calculateWages1;
+        public int calculateWages2;
+        public static void calWageTillCondition()
+        {
+            WagesCondition obj1 = new WagesCondition();
+            EmployWage obj2 = new EmployWage();
+
+            for (int count = 1; count < obj1.hrs; count++)
+            {
+
+                obj1.calculateWages1 = count * obj2.empWagePerHour;
+                Console.WriteLine("Total Working Hour wage for a Month is : {0}", +obj1.calculateWages1);
+                if (count < obj1.dayHrs)
+                {
+                    obj1.calculateWages2 = count * obj2.empWagePerHour;
+                    Console.WriteLine("Total Working Day wage for a Month is : {0}", +obj1.calculateWages2);
+
+                }
+
+            }
+        }
+    }
+}
