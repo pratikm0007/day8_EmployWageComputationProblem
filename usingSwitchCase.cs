aaccbd5b509412842b187ff3b283f02dd02c1b4a
pratikm0007 <pratikpmondal07@gmail.com>
2022-02-28 00:00:53 +0530

    push switch file

---
+++ usingSwitchCase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EmployWageComputationProblem
+{
+    public class usingSwitchCase
+    {
+        /// <summary>
+        /// Uses the switch case.
+        /// </summary>
+        public static void useSwitchCase()
+        {
+            Random random = new Random();
+            int presenty = random.Next(2);
+            switch (presenty)
+            {
+                case 1:
+                    Console.WriteLine("Employ is Present");
+                    EmployWage.CalculateEmpWage();
+                    break;
+                default:
+                    Console.WriteLine("Employ is Absent");
+                    Console.WriteLine("Employ per Day Wage is : 0");
+                    break;
+            }
+        }
+    }
+}
